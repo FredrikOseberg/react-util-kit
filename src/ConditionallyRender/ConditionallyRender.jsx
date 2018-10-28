6b2b4d7e40bebb75f3f14d5e462a5675fd9cb9cb
Fredrik Strand Oseberg <fredrik.no@gmail.com>
2018-10-28 22:54:31 +0100

    Added Dropdown to project

--- src/ConditionallyRender/ConditionallyRender.jsx
+++ src/ConditionallyRender/ConditionallyRender.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 class ConditionallyRender extends Component {
   render() {
     const { ifTrue, show, elseShow } = this.props;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import ConditionallyRender from './ConditionallyRender/ConditionallyRender.jsx';
 import Dropdown from './Dropdown/Dropdown.jsx';
 
 export { ConditionallyRender };
-export { Dropdown };
 
 const reactUtils = {
   ConditionallyRender,
